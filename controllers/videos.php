1ff27a188070ffd71711d8b3ad0f2022ffc94c15
neggmmm <96288489+neggmmm@users.noreply.github.com>
2024-06-07 23:05:06 +0300

    project

---
+++ controllers/videos.php
@@ -0,0 +1,11 @@
+<?php include('fhead.php');  ?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 text-center">
+
+			<h3> Welcome Faculty : <a href="welcomefaculty.php" ><span style="color:#FF0004"> </span></a> </h3>
+			<a href="addvideos.php"><button  href="" type="submit" class="btn btn-primary" style="border-radius:0%">Add Videos</button></a>
+			<a href="managevideos.php"><button  href="" type="submit" class="btn btn-primary" style="border-radius:0%">Manage Videos</button></a>
+			</div>
+	</div>
+	<?php include('allfoot.php');  ?>
